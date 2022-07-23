1fa7cc0e5fdc2d03ad3a417dc68da99cac8d6f66
BekovaDeniza <bekova_02@inbox.ru>
2022-07-23 23:05:14 +0300

    fixed parser, added data from api

--- amwine/amwine/spiders/prodpar.py
+++ amwine/amwine/spiders/prodpar.py
@@ -17,38 +17,45 @@ class ProdparSpider(scrapy.Spider):
     def parse(self, response):
         for i in self.section_ids:
             yield scrapy.Request(self.api, headers=self.headers,
-                body=f'json=y&params%5BIBLOCK_TYPE%5D=catalog&params%5BIBLOCK_ID%5D=2&params%5BSECTION_ID%5D={i}'
-                     f'&params%5BPRICE_CODE%5D=CFO&params%5BPAGE_ELEMENT_COUNT%5D=18',
-                callback=self.parse_page, meta={'section_id': i})
+                                 body=f'json=y&params%5BIBLOCK_TYPE%5D=catalog&params%5BIBLOCK_ID%5D=2&params%5B'
+                                      f'SECTION_ID%5D={i}&params%5BPRICE_CODE%5D=CFO&params%5BFILTER_NAME%5D=arrF'
+                                      f'ilterCatalog&params%5BSORT_ORDER%5D=ASC&params%5BSORT_FIELD%5D=SORT',
+                                 callback=self.parse_page, meta={'section_id': i})
 
     def parse_page(self, response):
         """
         Делаем повторный запрос, с количеством всех товаров.
         """
-        data = json.loads(response.body)
-        page_element_count = data["productsTotalCount"]
+        # data = json.loads(response.body)
+        # page_element_count = data["productsTotalCount"] # секунду назад работало, а сейчас нет
+        page_element_count = '25'  # задаем в ручную количество элементов
         section_id = response.meta['section_id']
         yield scrapy.Request(self.api,
                              headers=self.headers,
                              body=f'json=y&params%5BIBLOCK_TYPE%5D=catalog&params%5BIBLOCK_ID%5D=2&params%5BSECTION_ID%5'
-                                  f'D={section_id}&params%5BPRICE_CODE%5D=CFO&params%5BPAGE_ELEMENT_COUNT%5'
-                                  f'D={page_element_count}',
+                                  f'D={section_id}&params%5BPRICE_CODE%5D=CFO&params%5BPAGE_ELEMENT_COUNT%5D='
+                                  f'{page_element_count}&params%5BFILTER_NAME%5D=arrFilterCatalog&params%5BSORT_ORDER'
+                                  f'%5D=ASC&params%5BSORT_FIELD%5D=SORT',
                              method='POST',
                              callback=self.parse_links
                              )
 
     def parse_links(self, response):
         """
-        Получаем все ссылки товаров.
+        Получаем все ссылки на продуты и парсим из api имеющуюся информацию.
         """
         result = json.loads(response.body)
         for product in result['products']:
+            temp_data = {'available': product['available'], 'id': product['id'], 'name': product['name'],
+                         'article': product['props']['article'], 'image': product['preview_picture'],
+                         'price': {
+                             'current': product['props']['middle_price_77'],
+                             'original': product['props']['old_price_77'],
+                             'sale': product['sale']
+                         }
+                         }
             url = response.urljoin(product['link'])
-            available = False
-            if product['available']:
-                available = True
-
-            yield scrapy.Request(url, callback=self.parse_product, meta={'available': available})
+            yield scrapy.Request(url, callback=self.parse_product, meta={'temp_data': temp_data})
 
     def parse_product(self, response):
         """
@@ -56,33 +63,28 @@ class ProdparSpider(scrapy.Spider):
         """
         item = Product()
         item['timestamp'] = time()
+        item['RPC'] = response.meta['temp_data']['id']
         item['url'] = response.url
-        item['title'] = response.css('div.catalog-element-info__title h1::text').get().strip()
+        item['title'] = response.meta['temp_data']['name']
         item['marketing_tags'] = []
         item['brand'] = ''
         item['section'] = [x.strip() for x in response.css('div.breadcrumbs a::text').getall()]
         item['variants'] = 1
-        try:
-            image = 'https://amwine.ru' + response.css('div.catalog-element-info__picture img::attr(src)').get()
-        except:
-            image = ''
-        try:
-            item['RPC'] = response.css('div.catalog-element-info__article span::text').get().split()[-1]
-        except:
-            item['RPC'] = ''
         item['assets'] = {
-            "main_image": image,
+            "main_image": 'https://amwine.ru' + response.meta['temp_data']['image'],
             "set_images": [],
             "view360": [],
             "video": []
         }
         item['price_data'] = {
-            'current': 0.0,
-            'original': 0.0,
+            'current': float(response.meta['temp_data']['price']['current']),
+            'original': float(response.meta['temp_data']['price']['original']),
             'sale_tag': ''
         }
+        sale = response.meta['temp_data']['price']['sale']
+        if sale: item['price_data']['sale_tag'] = 'Скидка ' + sale.replace('-', '')
         item['stock'] = {
-            'in_stock': response.meta['available'],
+            'in_stock': response.meta['temp_data']['available'],
             'count': 0
         }
         item['metadata'] = {
@@ -92,6 +94,7 @@ class ProdparSpider(scrapy.Spider):
             'Производитель': '',
             'Крепость': '',
             'Выдержка': '',
+            'Артикул': response.meta['temp_data']['article']
         }
         wine_params = [x.replace('\n', '').replace(' ', '') for x in
                        response.css('div.about-wine__param *::text').getall()]
@@ -104,19 +107,4 @@ class ProdparSpider(scrapy.Spider):
         description = [x.replace('\n', '') for x in response.css('div.about-wine__block.col-md-4 *::text').getall()]
         description = [x for x in description if x.replace(' ', '')]
         item['metadata']['__description'] = ': '.join(description)
-        price = response.css(
-            'div.catalog-element-info__price.catalog-element-info__price_detail.mobile-show *::text').getall()
-        price = [float(x.replace(' ', '')) for x in price if x.replace(' ', '').replace('.', '').isdigit()]
-        if len(price) == 1:
-            item['price_data'] = {
-                'current': price[0],
-                'original': price[0],
-                'sale_tag': ''
-            }
-        elif len(price) == 2:
-            item['price_data'] = {
-                'current': price[0],
-                'original': price[1],
-                'sale_tag': 'Скидка ' + str(int((price[1] - price[0]) // (price[1] / 100))) + '%'
-            }
         yield item
